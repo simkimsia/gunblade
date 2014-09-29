d37cbd423d0b6731e39bcf6ee8de97ae2876a93a
KimSia Sim <kimcity@gmail.com>
2014-09-30 01:12:38 +0800

    add in GunBlade class with VERSION as class constant

---
+++ src/GunBlade/GunBlade.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ * This file is part of the GunBlade Project for PHP.
+ * 
+ * @package GunBlade
+ * 
+ * @license http://opensource.org/licenses/MIT
+ * 
+ */
+namespace GunBlade;
+
+class GunBlade {
+	const VERSION = '0.0.0.1';
+}
